9c25f146498cbd2472b495a19f25058621f86035
Kasuhira <hheflerdevelopment@gmail.com>
2019-11-23 15:40:53 -0300

    Finished V1.0

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 
 var pl = '' ;
 var pc = '' ;
+var isOn = false ;
+var isChosen = false;
 //Function That sorts the computer choice--------------------------------------------------------------//
 function computerPlay (){
     let i = Math.floor(Math.random()*4);
@@ -14,35 +16,32 @@ function computerPlay (){
 }
 //Function that stores the player choice-----------------------------------------------------------------//
 function playerChoice(e) {
-    pl = e;
-    pc = computerPlay();
-    playGame(pl,pc);
-
+    if(isChosen == false){
+        pl = e;
+        pc = computerPlay();
+        playGame(pl,pc);
+    }
 }
 //Function that makes the animations--------------------------------------------------------------------//
 function playGame(playerC,computerC){
-    const playerSide = document.querySelector('.player-choice');
-    if(playerC == 'ROCK'){
-        playerSide.classList.add('rock');
-    }else if(playerC == 'PAPER'){
-        playerSide.classList.add('paper');
-    }else{
-        playerSide.classList.add('scissors');
+    if(isChosen == false){
+        const playerSide = document.querySelector('.player-choice');
+        const showdownButton = document.querySelector('.showdown-button');
+        if(playerC == 'ROCK'){
+            playerSide.classList.add('rock');
+        }else if(playerC == 'PAPER'){
+            playerSide.classList.add('paper');
+        }else{
+            playerSide.classList.add('scissors');
+        }
+        showdownButton.classList.add('showdown-button-playing');
+        console.log(playerC);
+        console.log(computerC);
+        console.log(isOn);
+        isOn = true;
+        isChosen = true;
     }
-    console.log(playerC);
 }
-
-
-
-
-
-
-
-
-
-
-
-
 //Function that settles the score-------------------------------------------------------------------------//
 function gameResult(player,computer){
     if (player == 'ROCK'){
@@ -71,3 +70,45 @@ function gameResult(player,computer){
         }
     }
 }
+const play = document.querySelector('.showdown-button');
+    play.addEventListener('click',() => {
+        if(isOn==true){
+            const resText = document.createElement('h2');
+                resText.textContent = gameResult(pl,pc) ;
+                document.querySelector('.showdown-button').appendChild(resText) ;
+
+                document.querySelector('.showdown-button').classList.add('win-text');
+                if(pc == 'ROCK'){
+                    document.querySelector('.computer-choice').classList.add('rock');
+                }else if(pc == 'PAPER'){
+                    document.querySelector('.computer-choice').classList.add('paper');
+                }else{
+                    document.querySelector('.computer-choice').classList.add('scissors');
+                }
+            isOn = false ;
+        }
+    });
+    function resetGame(){
+        if (isOn == false && isChosen == true){
+            if(pc == 'ROCK'){
+                document.querySelector('.computer-choice').classList.remove('rock');
+            }else if(pc == 'PAPER'){
+                document.querySelector('.computer-choice').classList.remove('paper');
+            }else{
+                document.querySelector('.computer-choice').classList.remove('scissors');
+            }
+        if(pl == 'ROCK'){
+            document.querySelector('.player-choice').classList.remove('rock');
+        }else if(pl == 'PAPER'){
+            document.querySelector('.player-choice').classList.remove('paper');
+        }else{
+            document.querySelector('.player-choice').classList.remove('scissors');
+        }
+        document.querySelector('.showdown-button').removeChild(document.querySelector('h2')) ;
+        pl = '';
+        pc ='';
+        isChosen = false;
+        document.querySelector('.showdown-button').classList.remove('showdown-button-playing');
+        
+        }
+    }
